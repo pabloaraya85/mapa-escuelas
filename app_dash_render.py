5cdd9e92c3418b03af162788dc487497d7854b40
pabloaraya85 <araya.cortes.pablo@gmail.com>
2025-06-07 21:33:46 -0400

    app_dash_render.py

--- app_dash_render.py
+++ app_dash_render.py
@@ -105,4 +105,5 @@ def actualizar_vista(rbd_sel, radio):
     return fig, f"HHI (radio {radio} m): {hhi}", tabla
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
+
